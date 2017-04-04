0b4ca86276044c0bd0702a37c6600c6761e8407c
Balasubramania Sharma V <balasubramania.sharmav@cognizant.com>
2017-04-04 23:22:02 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,14 +1,27 @@
+// Get dependencies
+// https://scotch.io/tutorials/mean-app-with-angular-2-and-the-angular-cli
+// https://expressjs.com/en/starter/static-files.html
+
 const express = require('express');
 const path = require('path');
+const http = require('http');
+const bodyParser = require('body-parser');
+
 const app = express();
 
-app.set('port', (process.env.PORT || 3000));
+// Parsers for POST data
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Point static path to src
 app.use(express.static(path.join(__dirname, 'src')));
+
+app.set('port', (process.env.PORT || 3000));
 app.use('/node_modules', express.static(__dirname + '/node_modules'));
 
 
 app.get('*', function (req, res, next) {
-    res.sendFile(path.join(__dirname + '/src/index.html'));
+    res.sendFile(path.join(__dirname + 'src/index.html'));
 });
 
 // Start the app by listening on the default
