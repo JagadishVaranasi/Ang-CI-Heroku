9484dbad9c9bcc7eb4cafb4dc503910a6b469225
426111 <balasubramania.sharmav@cognizant.com>
2017-04-04 15:48:42 +0530

    added procfile server.js

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.set('port', (process.env.PORT || 3000));
+app.use(express.static(path.join(__dirname, 'src')));
+app.use('/node_modules', express.static(__dirname + '/node_modules'));
+
+
+
+// Start the app by listening on the default
+// Heroku port
+app.listen(app.get('port'),function() {    
+    console.log('Node server listening on port '+ app.get('port'));
+});
+
+module.exports = app;
