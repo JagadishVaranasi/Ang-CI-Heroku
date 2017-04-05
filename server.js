52597051f22babbb648ed9d7540bed709d92550a
426111 <balasubramania.sharmav@cognizant.com>
2017-04-05 18:13:32 +0530

    node integration

--- server.js
+++ server.js
@@ -1,18 +1,30 @@
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser');
 const app = express();
 
-app.set('port', (process.env.PORT || 3000));
+// Parsers for POST data
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+// Point static path to src
 app.use(express.static(path.join(__dirname, 'src')));
 app.use('/node_modules', express.static(__dirname + '/node_modules'));
 
 
-app.get('*', function (req, res, next) {
-    res.sendFile(path.join(__dirname + '/src/index.html'));
+app.get('/', function (req, res, next) {
+    res.render('index.html');
 });
 
+//Views
+app.set('views',path.join(__dirname,'src'));
+app.set('view engine','ejs');
+app.engine('html',require('ejs').renderFile);
+
+
 // Start the app by listening on the default
-// Heroku port
+// Heroku port or 3000
+app.set('port', (process.env.PORT || 3000));
 app.listen(app.get('port'),function() {    
     console.log('Node server listening on port '+ app.get('port'));
 });
